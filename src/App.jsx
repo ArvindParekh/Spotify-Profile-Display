fbd18790bbff293f6cb37cff1d866aa2f916752f
Arvind <aruparekh@gmail.com>
2024-01-24 07:37:47 +0530

    fix(ui): Remake the entire UI and change to spotify theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { getAccessToken } from "./utils/spotifyAuth";
 
 const SpotifyComponent = () => {
   const [profileData, setProfileData] = useState(null);
-  let renderRef = true;
+  // let renderRef = true;
 
   useEffect(() => {
     async function render() {
-      if (renderRef) {
-        await getToken();
-      }
+      // if (renderRef) {
+      await getToken();
+      // }
     }
     render();
   }, []);
@@ -18,22 +18,55 @@ const SpotifyComponent = () => {
   async function getToken() {
     const profile = await getAccessToken();
     setProfileData(profile);
-    renderRef = false;
-    console.log(profile);
+    // renderRef = false;
+    console.log(profileData);
   }
 
   return (
     <>
       {profileData && (
-        <>
-          <h1>Here are your top 10 artists!</h1>
+        <aside className="bg-white shadow-2xl flex flex-col border border-[#d6d6d6] rounded-3xl items-center w-[70%] h-[70%] my-auto justify-evenly">
+          <h1 className="font-bold text-6xl tracking-tight leading-[1.1]">
+            Here are your top 20 artists
+          </h1>
 
-          <ol>
-            {profileData.map((artist, index) => {
-              return <li key={index}>{artist.name}</li>;
-            })}
-          </ol>
-        </>
+          {/* <ol> */}
+          {console.log(profileData)}
+          <div className="flex items-center w-[100%] pl-40">
+            <ol className="w-1/4 text-muted-foreground list-decimal">
+              {profileData.slice(0,5).map((artist, index) => {
+                return <li className="text-xl p-2" key={index}>
+                  {artist.name}
+                </li>;
+              })}
+            </ol>
+
+            <ol className="w-1/4 text-muted-foreground list-decimal" start={6}>
+              {profileData.slice(5,10).map((artist, index) => {
+                return <li className="text-xl p-2" key={index}>
+                  {artist.name}
+                </li>;
+              })}
+            </ol>
+
+            <ol className="w-1/4 text-muted-foreground list-decimal" start={11}>
+              {profileData.slice(10,15).map((artist, index) => {
+                return <li className="text-xl p-2" key={index}>
+                  {artist.name}
+                </li>;
+              })}
+            </ol>
+
+            <ol className="w-1/4 text-muted-foreground list-decimal" start={15}>
+              {profileData.slice(15).map((artist, index) => {
+                return <li className="text-xl p-2" key={index}>
+                  {artist.name}
+                </li>;
+              })}
+            </ol>
+            {/* </ol> */}
+          </div>
+        </aside>
       )}
     </>
   );
